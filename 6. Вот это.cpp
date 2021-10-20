6437e29d365e4d9f686f454ecff9b36ee4fad4ea
Melondre <91074233+Melondre@users.noreply.github.com>
2021-10-20 13:47:19 +0300

    :)

---
+++ 6. Вот это.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+	float x, A, a;
+
+	cout << "Veddite A:";
+	cin >> A;
+
+	x = 0;
+	a = (A*A*A*A)*pow(A,4) - x;
+
+	cout << "Znachenie A:" << a << endl;
+}
