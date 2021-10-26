d5b1b1db7b50f320e74412f448145f86b0b23329
Melondre <91074233+Melondre@users.noreply.github.com>
2021-10-26 22:09:28 +0300

    :)

---
+++ 1. Определить значение этого же угла в радианах.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	float x, o, P, y;
+	P = 3, 14;
+
+	cout << "Veddite x: ";
+	cin >> x;
+
+	o = (x * P) / 180;
+
+	cout << "Otvet: " << o << endl;
+	return 0;
+
+}
