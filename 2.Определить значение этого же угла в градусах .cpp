8e0618103ed9bd6bf163daa1ba470da0e712d8c8
Melondre <91074233+Melondre@users.noreply.github.com>
2021-10-26 22:11:13 +0300

    :)

---
+++ 2.Определить значение этого же угла в градусах .cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	float x, o, P, y;
+	P = 3, 14;
+
+	cout << "Veddite x: ";
+	cin >> x;
+
+	o = (x * 180) / P;
+
+	cout << "Otvet: " << o << endl;
+	return 0;
+
+}
