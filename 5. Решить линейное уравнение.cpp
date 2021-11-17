b8bd0817860fe7bfbf1f3f442adc518df7f1000a
Melondre <91074233+Melondre@users.noreply.github.com>
2021-11-17 13:12:05 +0300

    :)

---
+++ 5. Решить линейное уравнение.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	float A, B, O;
+
+	cout << "Veddite A: ";
+	cin >> A;
+	cout << "Veddite B: ";
+	cin >> B;
+
+	O = -1 * B / A;
+
+	cout << "Otvet: " << O << endl;
+
+	return 0;
+
+}
