5c72b554ebf7c8edf9b4a52be38a2675a9765dbb
Melondre <91074233+Melondre@users.noreply.github.com>
2021-12-29 04:16:56 +0300

    :)

---
+++ 2.Найти количество отрезков B.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  
+    int a, b, c;
+  
+    cout << "a:" << endl;
+    cin >> a;
+    cout << "b:" << endl;
+    cin >> b;
+  
+    c = a / b;
+  
+    cout << "Kol - vo B otrezkov, placed on otrezke A:" << c;
+
+}
