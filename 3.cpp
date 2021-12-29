a4a07c3e3b2d6233203cff306a1fe83651ddc1e5
Melondre <91074233+Melondre@users.noreply.github.com>
2021-12-29 12:35:10 +0300

    :)

---
+++ 3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	int K, m, N;
+	cout << "K:";
+	cin >> K;
+	cout << endl;
+	cout << "N:";
+	cin >> N;
+	cout << endl;
+
+	N = N - 2;
+	m = ((K + N) % 7) + 1;
+
+	cout << "number of nedeyla:" << m;
+}
