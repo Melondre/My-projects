a85917e74df0b0a6c45edf075f545b033acaa34d
Melondre <91074233+Melondre@users.noreply.github.com>
2021-12-29 03:47:54 +0300

    :)

--- 2.Определить значение этого же угла в градусах .cpp
+++ 2.Определить значение этого же угла в градусах .cpp
@@ -1,18 +1,12 @@
-#include<iostream>
+#include <stdio.h>
 
-using namespace std;
-
-int main()
+int main(void)
 {
-	float x, o, P, y;
-	P = 3, 14;
+    float a;
+    printf("a:");
+    scanf_s("%f", &a);
 
-	cout << "Veddite x: ";
-	cin >> x;
+    printf("%f\n", (a * 180) / 3.14);
 
-	o = (x * 180) / P;
-
-	cout << "Otvet: " << o << endl;
-	return 0;
-
+    return 0;
 }
